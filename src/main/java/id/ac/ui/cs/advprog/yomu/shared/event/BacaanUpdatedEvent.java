a7c417c9c98c5cb07b64634c388132026f194894
Raithser <m.adella@ui.ac.id>
2026-05-22 19:58:16 +0700

    add BacaanUpdatedEvent

---
+++ src/main/java/id/ac/ui/cs/advprog/yomu/shared/event/BacaanUpdatedEvent.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.yomu.shared.event;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record BacaanUpdatedEvent(
+    UUID bacaanId,
+    String title,
+    String action,
+    Instant occurredAt
+) {}
